91855c27a6afb202d7221cf800d6ccaf6a065335
anthonyexe <47644214+anthonyexe@users.noreply.github.com>
2023-02-20 21:09:30 -0500

    Add files via upload

--- MontyHall.java
+++ MontyHall.java
@@ -1,42 +1,71 @@
 import java.util.*;
 
+
 public class MontyHall {
 
 	public double noChange(int n) {
-		
+		Random rand = new Random();
 		double count = 0;
 		double probability;
-		
-		ArrayList<Integer> scenario1 = new ArrayList<Integer>();
-		ArrayList<Integer> scenario2 = new ArrayList<Integer>();
-		ArrayList<Integer> scenario3 = new ArrayList<Integer>();
-		Collections.addAll(scenario1, 1, 2, 2);
-		Collections.addAll(scenario2, 2, 1, 2);
-		Collections.addAll(scenario3, 2, 2, 1);
-		
 		for (int i = 0; i < n; i++) {
-			int randomScenario;
-			int answer;
-			int reveal;
-			// 1.) Change random integer generation method
-			// 2.) Assign reveal a random integer corresponding to the available '2's' in each scenario
-			// 3.) Update if statement(s) to include reveal 
-			randomScenario = (int)(Math.random() * ((3 - 1) + 1)) + 1;
-			answer = (int)(Math.random() * 3);
+			ArrayList<Integer> scenario = new ArrayList<Integer>();
+			Collections.addAll(scenario, 2, 2, 2);
+			int carIndex = rand.nextInt(3);
+			scenario.set(carIndex, 1);
 			
-			switch (randomScenario) {
-			case 1:
-				if (scenario1.get(answer) == 1)
-					count++;
-			case 2:
-				if (scenario2.get(answer) == 1)
-					count++;
-			case 3:
-				if (scenario3.get(answer) == 1)
-					count++;
+			int guess = rand.nextInt(3);
+			/*
+			if (carIndex == 0) {
+				scenario.remove((int)(Math.random() * 2 + 1));
 			}
+			else if (carIndex == 1) {
+				int randomDecision = rand.nextInt(2);
+				if (randomDecision == 0)
+					scenario.remove(0);
+				else
+					scenario.remove(2);
+			}
+			else
+				scenario.remove(rand.nextInt(2));
+			*/
+			if (carIndex == guess)
+				count++;
 		}
-		probability = count / n;
+		
+		probability  = count / n;
+		return probability;
+	}
+	
+	public double withChange(int n) {
+		Random rand = new Random();
+		double count = 0;
+		double probability;
+		for (int i = 0; i < n; i++) {
+			ArrayList<Integer> scenario = new ArrayList<Integer>();
+			Collections.addAll(scenario, 2, 2, 2);
+			int carIndex = rand.nextInt(3);
+			scenario.set(carIndex, 1);
+			
+			int guess = rand.nextInt(3);
+			
+			if (carIndex == 0) {
+				scenario.remove((int)(Math.random() * 2 + 1));
+			}
+			else if (carIndex == 1) {
+				int randomDecision = rand.nextInt(2);
+				if (randomDecision == 0)
+					scenario.remove(0);
+				else
+					scenario.remove(2);
+			}
+			else
+				scenario.remove(rand.nextInt(2));
+			
+			//if (carIndex == guess)
+				//count++;
+		}
+		
+		probability  = count / n;
 		return probability;
 	}
 }
