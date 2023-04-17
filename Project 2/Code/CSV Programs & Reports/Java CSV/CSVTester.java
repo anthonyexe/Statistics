ae79b83cfcbd4b706cd2dd1eb8497acbb555f4af
anthonyexe <47644214+anthonyexe@users.noreply.github.com>
2023-04-16 22:22:25 -0400

    Add files via upload

---
+++ Project 2/Code/CSV Programs & Reports/Java CSV/CSVTester.java
@@ -0,0 +1,10 @@
+
+public class CSVTester {
+	public static void main(String[] args) {
+		CSV commas = new CSV();
+		
+		commas.plotter("xyValues", 1, 500, 0.5);
+		commas.salter("xyValues.csv", 0, 100000);
+		commas.smoother("saltedxyValues.csv", 25);
+	}
+}
