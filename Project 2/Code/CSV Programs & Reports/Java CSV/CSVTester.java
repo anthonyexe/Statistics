f5907be18f70c9ddb6eb3d238f09d2cd23aaed4a
anthonyexe <47644214+anthonyexe@users.noreply.github.com>
2023-04-21 11:22:45 -0400

    Update CSVTester.java

--- Project 2/Code/CSV Programs & Reports/Java CSV/CSVTester.java
+++ Project 2/Code/CSV Programs & Reports/Java CSV/CSVTester.java
@@ -1,10 +1,18 @@
 //Anthony D'Alessandro
 public class CSVTester {
 	public static void main(String[] args) {
+		//Create a new CSV object
 		CSV commas = new CSV();
 		
-		commas.plotter("xyValues", 1, 500, 0.5);
-		commas.salter("xyValues.csv", 0, 100000);
-		commas.smoother("saltedxyValues.csv", 25);
+		//Call the plotter method using a minimum x-value of 1, maximum x-value of 50, and an increment value of 0.5
+		commas.plotter("xyValues", 1, 50, 0.5);
+		
+		/*Call the salter method using the x and y values generated from the plotter method with a minimum salt value of 0 and
+		a maximum salt value of 250
+		*/
+		commas.salter("xyValues.csv", 0, 250);
+		
+		//Call the smoother method using the salted x and y values generated from the salter method with a window value of 8
+		commas.smoother("saltedxyValues.csv", 8);
 	}
 }
